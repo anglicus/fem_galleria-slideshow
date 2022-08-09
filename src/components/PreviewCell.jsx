f9878b35ff2eef3dde9a380c7c4d52483731622e
anglicus <anglicus@yahoo.com>
2022-08-09 11:14:07 +0900

    grid layout for tablet and desktop

--- src/components/PreviewCell.jsx
+++ src/components/PreviewCell.jsx
@@ -1,13 +1,19 @@
 const PreviewCell = (props) => {
   const cellImage = new Image();
   cellImage.src = props.source;
-  const rowSpan = Math.floor(cellImage.height / 10);
-
+  const rowSpan = Math.floor(cellImage.height / 10) + 4;
+  const cellStyle = {
+    gridRow: "span " + rowSpan,
+    "--desktop-col": props.desktopColumn,
+    "--tablet-col": props.tabletColumn,
+  };
   return (
-    <div className="preview-cell" style={{ gridRowStart: "span " + rowSpan }}>
+    <div className="preview-cell" style={cellStyle}>
       {<img src={`${props.source}`} alt={props.name}></img>}
-      <h2>{props.title}</h2>
-      <p>{props.artist}</p>
+      <div className="preview-cell__text-div">
+        <h2>{props.title}</h2>
+        <p>{props.artist}</p>
+      </div>
     </div>
   );
 };
--- src/components/PreviewGrid.jsx
+++ src/components/PreviewGrid.jsx
@@ -2,14 +2,19 @@
 
 import PreviewCell from "./PreviewCell";
 
+const desktopColumns = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 4, 1, 2, 3, 4];
+const tabletColumns = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 2, 1, 2, 1, 2];
+
 const PreviewGrid = (props) => {
   return (
     <div className="preview-grid">
-      {props.paintings.map((painting) => (
+      {props.paintings.map((painting, i) => (
         <PreviewCell
           source={painting.images.thumbnail}
           title={painting.name}
           artist={painting.artist.name}
+          desktopColumn={desktopColumns[i]}
+          tabletColumn={tabletColumns[i]}
           key={painting.name}
         />
       ))}
