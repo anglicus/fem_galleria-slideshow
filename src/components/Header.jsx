82ea5490e8197d3e4121da0fda7dec734ca395c6
anglicus <anglicus@yahoo.com>
2022-08-11 13:39:08 +0900

    invisible h1 in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ const Header = (props) => {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="galleria logo" />
+      <h1 className="header__heading">galleria.</h1>
       <button
         className="header__button header-link button--linkstyle"
         onClick={props.pageTurning ? null : props.toggleSlideShow}
